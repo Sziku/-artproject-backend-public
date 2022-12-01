b94574301fe600b8e2702bbb768e9ed61a181491
Sziku <105989158+Sziku@users.noreply.github.com>
2022-12-01 11:35:36 +0100

    Initial commit

---
+++ src/main/java/com/codecool/fileshare/repository/ImageJdbcRepository.java
@@ -0,0 +1,149 @@
+package com.codecool.fileshare.repository;
+
+import com.codecool.fileshare.model.Image;
+import org.springframework.stereotype.Component;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component("jdbc")
+public class ImageJdbcRepository implements ImageRepository{
+    static final String DB_TYPE = "jdbc:postgresql";
+
+    static final String ADDRESS = System.getenv("host");
+
+    static final int PORT = Integer.parseInt(System.getenv("port"));
+
+    static final String DB_NAME = System.getenv("dbname");
+
+    static final String DB_URL = DB_TYPE + "://" + ADDRESS + ":" + PORT + "/" + DB_NAME;
+
+    static final String USER = System.getenv("dbusername");
+
+    static final String PASS = System.getenv("dbpassword");
+    @Override
+    public String storeImageFile(String title, String description, String owner, byte[] content, String extension) {
+        //TODO Balazs
+        final String SQL = "insert into image(title, owner, description, content, extension) values(?,?,?,?,?);";
+        String id  = null;
+        try(Connection con = DriverManager.getConnection(DB_URL, USER, PASS)){
+            PreparedStatement st = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, title);
+            st.setString(2, owner);
+            st.setString(3, description);
+            st.setBytes(4, content);
+            st.setString(5, extension);
+
+            st.executeUpdate();
+
+            ResultSet rs = st.getGeneratedKeys();
+            rs.next();
+            id  = rs.getString(1);
+            return id;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean checkOwner(String owner, String id) {
+        //  where id = cast(? as uuid)
+        final String sql = "SELECT owner FROM image WHERE id = CAST(? as uuid);";
+
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)){
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1,id);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            return resultSet.getString(1).equals(owner);
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<Image> getAll(String owner) {
+        final String SQL = "select id, title, owner, description, content, extension from image where owner = ?;";
+
+        try(Connection con = DriverManager.getConnection(DB_URL, USER, PASS)){
+            PreparedStatement st = con.prepareStatement(SQL);
+            st.setString(1, owner);
+
+            ResultSet rs = st.executeQuery();
+            List<Image> imageList = new ArrayList<>();
+            while (rs.next()){
+                imageList.add(new Image(rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getBytes(5),
+                        rs.getString(6)));
+            }
+
+            return imageList;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(String uuid, String owner) {
+        final String sql = "DELETE FROM image WHERE id = CAST(? as uuid) AND owner = ?;";
+
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)){
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1,uuid);
+            statement.setString(2,owner);
+
+            statement.executeUpdate();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateImage(String id, String title, String description, String owner) {
+        //TODO Balazs
+        final String SQL = "update image set title = ?, owner = ?, description = ? where id = cast(? as uuid) ;";
+
+        try(Connection con = DriverManager.getConnection(DB_URL, USER, PASS)){
+            PreparedStatement st = con.prepareStatement(SQL);
+            st.setString(1, title);
+            st.setString(2, owner);
+            st.setString(3, description);
+            st.setString(4, id);
+
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public byte[] getImageFile(String id) {
+        final String SQL = "select content from image where id = cast(? as uuid) ;";
+
+        try(Connection con = DriverManager.getConnection(DB_URL, USER, PASS)){
+            PreparedStatement st = con.prepareStatement(SQL);
+            st.setString(1, id);
+
+            ResultSet rs = st.executeQuery();
+            rs.next();
+
+            return rs.getBytes(1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+}
---
+++ src/main/java/com/codecool/fileshare/service/ImageService.java
@@ -0,0 +1,67 @@
+package com.codecool.fileshare.service;
+
+import com.codecool.fileshare.dto.ImageDataDTO;
+import com.codecool.fileshare.dto.ImageUpdateDTO;
+import com.codecool.fileshare.model.Image;
+import com.codecool.fileshare.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ImageService {
+
+    @Autowired
+    @Qualifier("jdbc")
+    private ImageRepository imageRepository;
+
+    public List<ImageDataDTO> getAll(String user){
+        List<ImageDataDTO> imageDataDTOList = new ArrayList<>();
+        for(Image image : imageRepository.getAll(user)){
+            String imgurl = System.getenv("url") + "/api/artwork/" + image.getId() + "." +image.getExtension();
+
+            imageDataDTOList.add(new ImageDataDTO(
+                    image.getId(),
+                    image.getTitle(),
+                    image.getDescription(),
+                    imgurl
+            ));
+        }
+        return imageDataDTOList;
+    }
+
+    public boolean checkOwner(String owner, String id){ //Adrian
+        return imageRepository.checkOwner(owner, id);
+    }
+
+    public void delete(String id, String owner) { //Adrian
+        imageRepository.delete(id, owner);
+    }
+
+    public void updateCategory(String id ,ImageUpdateDTO imageUpdateDTO, String owner) {
+        //TODO Balazs
+        imageRepository.updateImage(id,imageUpdateDTO.getTitle(),imageUpdateDTO.getDescription(),owner);
+    }
+
+    public byte[] getImageFile(String filename) {
+        //TODO Balazs help: filename is for example 41d6608d-0803-4239-9235-09f902fbf705.jpg split("\\.")
+        String[] fileNameSplit = filename.split("\\.");
+        return imageRepository.getImageFile(fileNameSplit[0]);
+
+    }
+
+    public String storeFile(MultipartFile file, String title, String description, String owner) {
+        String[] fileNameSplit = file.getOriginalFilename().split("\\.");
+        try {
+            return imageRepository.storeImageFile(title,description,owner,file.getBytes(),fileNameSplit[1]);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
